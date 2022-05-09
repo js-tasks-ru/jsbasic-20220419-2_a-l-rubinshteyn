18918679f66a4edb532fb9ef5d6ed25921ab6c0b
Anatoly Rubinshteyn <a.l.rubinshteyn@gmail.com>
2022-05-09 14:15:53 +0300

    Задачи 4 урока

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,27 @@
+const map = {
+  'Name': 0,
+  'Age': 1,
+  'Gender': 2,
+  'Status': 3
+}
+
 function highlight(table) {
-  // ваш код...
+  for (let index = 1; index < table.rows.length; index++) {
+
+    const row = table.rows[index];
+
+    if (row.cells[map.Status].hasAttribute('data-available')) {
+      let available = row.cells[map.Status].dataset.available;
+      row.classList.add(available === 'true'  ? 'available' : 'unavailable');
+    } else {
+      row.hidden = true;
+    }
+
+    let gender = row.cells[map.Gender].innerText;
+    row.classList.add(gender === 'm' ? 'male' : 'female');
+
+    if (row.cells[map.Age].innerText < 18) {
+      row.style.textDecoration = "line-through";
+    }
+  }
 }
