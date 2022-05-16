3918f86214ef48d7f9324c142557949453593678
Anatoly Rubinshteyn <a.l.rubinshteyn@gmail.com>
2022-05-16 15:48:39 +0300

    задачи 5 урока

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,40 @@
 function initCarousel() {
-  // ваш код...
+  let arrowLeft = document.querySelector('.carousel__arrow_left');
+  let arrowRight = document.querySelector('.carousel__arrow_right');
+  
+  //при открытии страницына первом слайде скрываем левую стрелку
+  arrowLeft.style.display = 'none';
+
+  arrowLeft.addEventListener('click', function () {
+    translateSlider('left', arrowLeft, arrowRight);
+  });
+
+  arrowRight.addEventListener('click', function () {
+    translateSlider('right', arrowLeft, arrowRight);
+  });
+}
+
+function translateSlider(direction, arrowLeft, arrowRight) {
+  let slidesCount = document.querySelectorAll('.carousel__slide').length;
+  let currentSlide = document.querySelector('.container').dataset.carouselHolder;
+
+  direction === 'right' ? currentSlide++ : currentSlide--;
+
+  switch (currentSlide) {
+    case 0:
+      arrowLeft.style.display = 'none';
+      break;
+    case slidesCount - 1:
+      arrowRight.style.display = 'none';
+      break;
+    default:
+      arrowRight.style.display = arrowLeft.style.display = '';
+      break;
+  }
+
+  let offset = (-1) * currentSlide * document.querySelector('.carousel__slide').offsetWidth;
+  document.querySelector('.carousel__inner').style.transform = 'translateX(' + offset + 'px)';
+
+  document.querySelector('.container').dataset.carouselHolder = currentSlide;
+
 }
